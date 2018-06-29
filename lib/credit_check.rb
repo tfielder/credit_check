d6f4fe1199560e09955b44a458ee3a4311234c7c
Tim Fielder <fiel1139@hotmail.com>
2018-06-28 22:46:59 -0600

    adds working credit_check_test

--- lib/credit_check.rb
+++
@@ -1,7 +0,0 @@
-require "./credit_check_class.rb"
-
-credit_check = CreditCheck.new
-credit_check.valid_number?(5541808923795240)
-credit_check.valid_number?(5541801923795240)
-credit_check.validation_output(5541808923795240)
-credit_check.validation_output(5541801923795240)
---
+++ test/credit_check_test.rb
@@ -0,0 +1,53 @@
+require "../lib/CreditCheck_class.rb"
+require "minitest/autorun"
+require "minitest/pride"
+#require "pry"
+
+
+class CreditCheckTest < Minitest::Test
+  def test_ininitiates_a_class
+    #assert CreditCheck.new
+    #require "pry"; binding.pry
+  end
+
+  def test_reverses_card_number
+    skip
+  end
+
+  def test_card_string_to_characters
+    skip
+  end
+
+  def test_converts_array_to_integers
+    skip
+  end
+
+  def test_doubles_every_other_integer
+    skip
+  end
+
+  def test_sums_double_digits
+    skip
+  end
+
+  def test_sums_the_array
+    skip
+  end
+
+  def test_checks_validity
+    skip
+  end
+
+  def test_algorithm
+    skip
+  end
+
+  def test_returns_boolean_value_for_validity
+    skip
+  end
+
+  def test_validation_output
+    skip
+  end
+
+end
