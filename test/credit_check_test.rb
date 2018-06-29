4aa8c7daf371ee6b778e86733d9f8c4507c262fe
Tim Fielder <fiel1139@hotmail.com>
2018-06-28 18:17:41 -0600

    arranges file structure

---
+++ test/credit_check_test.rb
@@ -0,0 +1,50 @@
+require "../lib/credit_check_class.rb"
+require "minitest/autorun"
+require "minitest/pride"
+
+class CreditCheck < Minitest::Test
+  def test_ininitiates_a_class
+    assert CreditCheck.new
+  end
+
+  def test_reverses_card_number
+    skip
+  end
+
+  def test_card_string_to_characters
+    skip
+  end
+
+  def test_converts_array_to_integers
+    skip
+  end
+
+  def test_doubles_every_other_integer
+    skip
+  end
+
+  def test_sums_double_digits
+    skip
+  end
+
+  def test_sums_the_array
+    skip
+  end
+
+  def test_checks_validity
+    skip
+  end
+
+  def test_algorithm
+    skip
+  end
+
+  def test_returns_boolean_value_for_validity
+    skip
+  end
+
+  def test_validation_output
+    skip
+  end
+
+end
